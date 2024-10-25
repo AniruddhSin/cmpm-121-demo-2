ccea75a4302d20e8138ee0a98be0cb081d52d53d
AniruddhSin <146397992+AniruddhSin@users.noreply.github.com>
2024-10-24 18:16:01 -0700

    Finished Step 1

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import "./style.css";
 
-const APP_NAME = "Hello";
+const APP_NAME = "Lets Get Sketchy";
 const app = document.querySelector<HTMLDivElement>("#app")!;
+//const canvas = document.querySelector<HTMLCanvasElement>("#canvas")!;
+
 
 document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+app.innerHTML = `<h1>${APP_NAME}</h1>`;
+
