226a22c565bfa55c6c22c5697ab406dc0e076d72
AniruddhSin <146397992+AniruddhSin@users.noreply.github.com>
2024-10-24 21:56:24 -0700

    Finished Step 5

--- src/main.ts
+++ src/main.ts
@@ -2,64 +2,84 @@ import "./style.css";
 
 const APP_NAME = "Lets Get Sketchy";
 const app = document.querySelector<HTMLDivElement>("#app")!;
+document.title = APP_NAME;
+app.innerHTML = `<h1>${APP_NAME}</h1>`;
 const canvas = document.querySelector<HTMLCanvasElement>("#canvas")!;
 const canvas_ctx = canvas.getContext("2d")!;
-const cursor = {
-    active: false,
-    x: 0,
-    y: 0
-};
+let mouseHeld: boolean = false;
 const drawing_changed = new Event("drawing_changed")
-interface Point{
+type Point = {
     x: number,
     y: number
 }
-const lines: Point[][]= [];
-let currentLine: Point[] = [];
-const redoStack: Point[][] = [];
+type Line = Point[];
+/*
+step 6:
+make a class that represents one "line" drawn by a single mousedown event
+add each of these "lines" to the redo line stack
+lines array should be an array of LineObjects
+these objects will have a starting point from when to draw and a display method to draw out the lines
+*/
+interface LineObject {
+    line: Line,
+    drag(point: Point): void,
+    display(ctx: CanvasRenderingContext2D): void
+}
 
-document.title = APP_NAME;
-app.innerHTML = `<h1>${APP_NAME}</h1>`;
+class MarkerLine implements LineObject{
+    line: Line;
+
+    constructor (point: Point){
+        this.line = [point];
+    }
+    drag(point: Point){
+        this.line.push(point);
+    }
+    display(ctx: CanvasRenderingContext2D): void {
+        ctx.beginPath();
+        if (this.line.length > 0){
+            const {x,y} = this.line[0];
+            ctx.moveTo(x,y);
+        }else{
+            return;
+        }
+        for (const point of this.line){
+            ctx.lineTo(point.x, point.y);
+        }
+        ctx.stroke();
+    }
+
+}
+const lines: MarkerLine[]= [];
+let currentLine: MarkerLine | null;
+const redoStack: MarkerLine[] = [];
 
 // Add mouse detection to Canvas
 canvas.addEventListener("mousedown", (e)=>{
-    cursor.active = true;
-    cursor.x = e.offsetX;
-    cursor.y = e.offsetY;
-    
+    mouseHeld = true;
+    currentLine = new MarkerLine({x: e.offsetX, y: e.offsetY});
     redoStack.length = 0;
-    currentLine.push({x: cursor.x, y: cursor.y});
     lines.push(currentLine);
 
     canvas.dispatchEvent(drawing_changed);
 })
 canvas.addEventListener("mousemove", (e)=>{
-    if (cursor.active){
-        cursor.x = e.offsetX;
-        cursor.y = e.offsetY;
-        currentLine.push({x: cursor.x, y:cursor.y});
+    if (mouseHeld){
+        currentLine!.drag({x: e.offsetX, y:e.offsetY})
 
         canvas.dispatchEvent(drawing_changed);
     }
 })
 canvas.addEventListener("mouseup", ()=>{
-    cursor.active = false;
-    currentLine = [];
+    mouseHeld = false;
+    currentLine = null;
 })
 
 // Add redraw event to canvas
 canvas.addEventListener("drawing_changed", ()=>{
     canvas_ctx.clearRect(0, 0, canvas.width, canvas.height);
-    for (const line of lines){
-        if (line.length >= 2){
-            canvas_ctx.beginPath();
-            const {x,y} = line[0];
-            canvas_ctx.moveTo(x, y);
-            for (const {x,y} of line){
-                canvas_ctx.lineTo(x,y)
-            }
-            canvas_ctx.stroke();
-        }
+    for (const line of lines){  // line is a MarkerLine object
+        line.display(canvas_ctx);
     }
 })
 
@@ -70,6 +90,7 @@ clearButton.innerHTML = "clear";
 clearButton.addEventListener("click", ()=>{
     canvas_ctx.clearRect(0, 0, canvas.width, canvas.height);
     lines.length = 0;
+    redoStack.length = 0;
 })
 document.body.append(clearButton);
 
@@ -83,7 +104,6 @@ undoButton.addEventListener("click", ()=>{
         canvas.dispatchEvent(drawing_changed);
     }
 })
-
 
 const redoButton = document.createElement("button");
 redoButton.innerHTML = "redo";
